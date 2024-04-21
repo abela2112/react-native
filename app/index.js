81bb81b1a85af75ee9250e39cf65683bc4b19a7b
Abel Ayalew Abebe <103965027+abela2112@users.noreply.github.com>
2024-04-22 02:56:30 +0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-p">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+      <Link href={"/Profile"}>Go to Profile</Link>
+      {/*  */}
+    </View>
+  );
+}
